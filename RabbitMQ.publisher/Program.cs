c8d4c1a24d8fce9562ed94ca3ab8536e039d129d
Adem Tunçalın <tuncalin@outlook.com>
2022-03-24 16:22:33 +0300

    Kuyruğa verinin yazılması, connect, channel, byte[], publish

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabbitMQ.publisher
 {
@@ -6,7 +8,33 @@ namespace RabbitMQ.publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://tpmxkawx:tP_TZFioZbqGBarCUE3Lsl4l-C8jDaed@fish.rmq.cloudamqp.com/tpmxkawx");
+
+
+            using var connection = factory.CreateConnection(); //Connection Gerçekleşir.
+
+
+            var channel = connection.CreateModel(); //Kanal Oluşur
+
+
+            //durable: true olursa fiziksel tutulur restart edilirse sorun olmaz, false olursa bellekte tutulur
+            //exclusive:true olursa kuyruğa sadece bu kanal üzerinden bağlanabilir başka bağlantı kabul etmez 
+            //autoDelete: Kuyruğa bağlı olan son subscribe bağlantısını koparırsa kuyruğu otomatik siler.
+            channel.QueueDeclare("hello-queue", true, false, false); //Kuyruk oluşur..
+
+            string mesaj = "hello world";
+
+
+            //Kuyruğa mesajlar, byte[] olarak gönderilir. 
+            var messageBody = Encoding.UTF8.GetBytes(mesaj);
+
+
+            channel.BasicPublish(string.Empty, "hello-queue", null, messageBody); //mesaj gönderilir. 
+
+            Console.WriteLine("Mesaj Gönderilmiştir.");
+            Console.ReadLine();
         }
     }
 }
