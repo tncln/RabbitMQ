dfa9aeead80e1148b791ed33141e0748d57aa736
Adem Tunçalın <tuncalin@outlook.com>
2022-03-29 09:01:49 +0300

    Topic Exchange Projesinin oluşturulması, Kullanım

---
+++ RabbitMQ.SubscriberTopicExchange/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RabbitMQ.SubscriberTopicExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://tpmxkawx:tP_TZFioZbqGBarCUE3Lsl4l-C8jDaed@fish.rmq.cloudamqp.com/tpmxkawx");
+
+            using var connection = factory.CreateConnection(); //Connection Gerçekleşir. 
+
+            var channel = connection.CreateModel(); //Kanal Oluşur
+
+
+
+
+            channel.BasicQos(0, 1, false);//false, Her subscriber a 1 1 gönderir Değer kaç ise, true; 10 adet ise,
+                                          //subscriber 5 ise 2şer şeklinde gönderir. 
+
+            //Kuyruktan veri okurken bu kısım silinebilir, eğer silinirse kuyruk okumaya çalıştığında eğer kuyruk
+            //ismi yoksa yeni oluşturur, silinmez ise kuyruğu bulamadığı durumda hata verir,
+            channel.QueueDeclare("hello-queue", true, false, false); //Kuyruk oluşur..
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = "direct-queue-Critical";
+
+            //autoAck:true olursa doğruda işlense yanlışta işlense kuyruktan siler. false olursa,
+            //kuyruktan silmez doğru işlerse silmek için haber verir
+            channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("Logları dinleniyor..");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen Mesaj:" + message);
+                File.AppendAllText("log-critical.txt", message + "\n");
+                channel.BasicAck(e.DeliveryTag, false);// kuyruktan okunan mesajın silinmesini sağlar. Başarılı şekilde işlendi artık kuyruktan sil denilir. 
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.publisherTopicExchange/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace RabbitMQ.publisherTopicExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://tpmxkawx:tP_TZFioZbqGBarCUE3Lsl4l-C8jDaed@fish.rmq.cloudamqp.com/tpmxkawx");
+
+
+            using var connection = factory.CreateConnection(); //Connection Gerçekleşir.
+
+
+            var channel = connection.CreateModel(); //Kanal Oluşur 
+
+            //channel.QueueDeclare("hello-queue", true, false, false); //Kuyruk oluşur..
+
+            channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+            Enum.GetNames(typeof(LogNames)).ToList().ForEach(x => {
+                var routeKey = $"route-{x}";
+                var queueName = $"direct-queue-{x}";
+                channel.QueueDeclare(queueName, true, false, false);
+                channel.QueueBind(queueName, "logs-direct", routeKey);
+            });
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+
+                LogNames log = (LogNames)new Random().Next(1, 5);
+
+                string mesaj = $"log-type : {log}";
+
+
+                //Kuyruğa mesajlar, byte[] olarak gönderilir. 
+                var messageBody = Encoding.UTF8.GetBytes(mesaj);
+
+                var routeKey = $"route-{log}";
+
+                channel.BasicPublish("logs-direct", routeKey, null, messageBody); //mesaj gönderilir. 
+
+                Console.WriteLine($"{mesaj}. Log Gönderilmiştir.");
+            });
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
