43ffb17b6a70bd1729998b2e383c56c48eec8ce0
Adem Tunçalın <tuncalin@outlook.com>
2022-03-29 14:13:47 +0300

    Complex Type eklenmesi

--- RabbitMQ.PublisherHeaderExchange/Program.cs
+++ RabbitMQ.PublisherHeaderExchange/Program.cs
@@ -1,8 +1,10 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 
 namespace RabbitMQ.PublisherHeaderExchange
 {
@@ -39,8 +41,13 @@ namespace RabbitMQ.PublisherHeaderExchange
 
                 var properties = channel.CreateBasicProperties();
                 properties.Headers = headers;
+                properties.Persistent = true; //mesajların kalıcı hale getirilmesi
 
-                channel.BasicPublish("header-exchange", string.Empty, properties,Encoding.UTF8.GetBytes("header mesajım"));
+                var product = new Product { Id=1,Name="Kalem", Price=100 , Stock=10};
+
+                var productJsonString = JsonSerializer.Serialize(product);
+
+                channel.BasicPublish("header-exchange", string.Empty, properties,Encoding.UTF8.GetBytes(productJsonString));
 
                 Console.WriteLine("Mesaj Gönderildi..");
 
